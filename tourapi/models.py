84b69a8f3390b56818d7d4f86ff6f8bcf14aa340
Christian Lurhakwa <irchristianscott@gmail.com>
2017-11-23 18:25:58 +0300

    rqrmt

--- tourapi/models.py
+++ tourapi/models.py
@@ -3,5 +3,9 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Model):
+class Tourist(models.Model):
+	full_name = models.CharField(max_length=100)
+
+
+class ServiceProviders(models.Model):
 	full_name = models.CharField(max_length=100)
